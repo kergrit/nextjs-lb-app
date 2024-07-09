56bca91d70866f6339db7ea29b9db516ab457f8b
Kergrit Robkop <kergrit@gmail.com>
2024-07-09 12:42:16 +0700

    Update basePath assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: 'build',
   output: 'standalone',
-  basePath: '/nextjs-base',
+  // basePath: '/nextjs-base',
   assetPrefix: 'https://az-demo-lb.digi-team.work/nextjs',
 };
 
